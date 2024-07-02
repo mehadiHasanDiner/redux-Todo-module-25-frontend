07bf237574e5aaec268b715b6dbbcaba90f4affd
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-07-02 17:33:25 +0600

    module done

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -2,9 +2,19 @@ import { useAppSelector } from "@/redux/hook";
 import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 import TodoFilter from "./TodoFilter";
+import { useGetTodosQuery } from "@/redux/api/api";
 
 const TodoContainer = () => {
-  const { todos } = useAppSelector((state) => state.todos);
+  //  * From Local state
+  // const { todos } = useAppSelector((state) => state.todos);
+
+  // * From server
+  const { data: todos, isLoading, isError, } = useGetTodosQuery(undefined);
+  console.log(todos)
+
+  if (isLoading) {
+    return <p>Loading ..........</p>
+  }
 
   return (
     <div>
@@ -18,8 +28,8 @@ const TodoContainer = () => {
             <p>There is no task pending</p>
         </div> */}
         <div className="h-full w-full space-y-3 rounded-lg bg-white p-5">
-          {todos.map((item) => (
-            <TodoCard {...item}/>
+          {todos?.map((item) => (
+            <TodoCard {...item} />
           ))}
         </div>
       </div>
